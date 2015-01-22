973f980e6d9be0076dee6674427e068b3b2d9d49
5903390@gmail.com <5903390@gmail.com@620c7109-8c42-3e95-abc8-313544137013>
2015-01-22 10:10:41 +0000

    Убраны лишние действия с gson. Теперь в сообщениях передаются сериализованные объекты.

---
+++ WebProbaPera/src/java/ru/nosov/client/WelcomeEntryPoint.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.nosov.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+import com.google.gwt.user.client.ui.DecoratorPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.web.bindery.autobean.shared.AutoBean;
+import com.google.web.bindery.autobean.shared.AutoBeanCodex;
+import com.google.web.bindery.autobean.shared.AutoBeanUtils;
+import ru.nosov.client.messages.types.TypeMessage;
+import ru.nosov.client.panels.PanelLogin;
+
+/**
+ * Главная страница.
+ * http://www.dokwork.ru/2014/02/daotalk.html#refactor
+ * @author Носов А.В.
+ */
+public class WelcomeEntryPoint implements EntryPoint {
+    
+    // Variables declaration
+    
+    // End of variables declaration
+    
+    /**
+     * Creates a new instance of MainEntryPoint
+     */
+    public WelcomeEntryPoint() {
+    }
+
+    /**
+     * The entry point method, called automatically by loading a module that
+     * declares an implementing class as an entry-point
+     */
+    @Override
+    public void onModuleLoad() {
+        RootPanel.get().add(new PanelLogin());
+    }
+}
---
+++ WebProbaPera/src/java/ru/nosov/client/messages/MsgService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.nosov.client.messages;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("msgServiceImpl")
+public interface MsgService extends RemoteService {
+    
+    /**
+     * Возвращает сообщение от сервера к клиенту
+     * @param msg сообщение от клиента к серверу
+     * @return сообщение от сервера к клиенту
+     */
+    public Message getMsg(Message msg);
+    
+}
